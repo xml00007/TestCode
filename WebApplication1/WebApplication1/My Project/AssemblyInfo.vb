315797c0cddc2ce99099babc356b94307ad198d3
xml00007 <xml00007@163.com>
2016-04-18 21:48:14 +0800

    添加目录
    第一次添加

---
+++ WebApplication1/WebApplication1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 检查程序集特性的值
+<Assembly: AssemblyTitle("WebApplication1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Sky123.Org")> 
+<Assembly: AssemblyProduct("WebApplication1")> 
+<Assembly: AssemblyCopyright("Copyright © Sky123.Org 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
+<Assembly: Guid("7428a784-d480-44f6-9eb4-42cd24d425e5")> 
+
+' 程序集的版本信息由下列四个值组成:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' 可以指定所有值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用 "*":
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
